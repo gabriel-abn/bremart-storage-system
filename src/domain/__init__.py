476c744dd614eb56f4472572de5ee6685aeab6a3
gabriel-abn <gabriel.ab.nascimento@gmail.com>
2023-07-20 11:24:10 -0300

    feat(Shipment): :sparkles: shipment entity class

--- src/domain/__init__.py
+++ src/domain/__init__.py
@@ -1,12 +1,15 @@
 from .common import DomainError, Entity
 from .distributor import Distributor, DistributorProps
 from .product import *
+from .shipment import Shipment, ShipmentProps
 
 __all__ = [
+    "DomainError",
+    "Entity",
     "Product",
     "ProductProps",
     "Distributor",
     "DistributorProps",
-    "DomainError",
-    "Entity",
+    "Shipment",
+    "ShipmentProps",
 ]
---
+++ src/domain/shipment.py
@@ -0,0 +1,40 @@
+from datetime import datetime
+from typing import TypedDict
+
+from src.domain.common import DomainError, Entity
+
+
+class ShipmentProps(TypedDict):
+    type: str
+    source: str
+    destiny: float
+    start_date: datetime
+    estimated_end_date: datetime
+    cargos: list[dict[{"purchase": str, "products": list[str]}]]
+
+
+class Shipment(Entity[ShipmentProps]):
+    def __init__(self, props: ShipmentProps, id: str) -> None:
+        super().__init__(props, id)
+
+    @staticmethod
+    def create(props: ShipmentProps, id: str):
+        if props["type"] not in ["air", "sea", "land"]:
+            raise DomainError("Shipment", "Type is not valid")
+
+        if props["source"] == props["destiny"]:
+            raise DomainError("Shipment", "Source and destiny are the same")
+
+        if props["cargos"] == []:
+            raise DomainError("Shipment", "Cargos are empty")
+
+        if (
+            props["start_date"] > props["estimated_end_date"]
+            or props["start_date"] == props["estimated_end_date"]
+        ):
+            raise DomainError(
+                "Shipment",
+                "Dates are not valid, start date must be before estimated end date",
+            )
+
+        return Shipment(props, id)
