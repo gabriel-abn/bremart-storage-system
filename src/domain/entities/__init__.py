9242b538db1df6e5efba6b0273d493e3aa19f052
gabriel-abn <gabriel.ab.nascimento@gmail.com>
2023-07-20 12:04:00 -0300

    feat(Storage): :sparkles: storage entity base class

--- src/domain/entities/__init__.py
+++ src/domain/entities/__init__.py
@@ -1,15 +1,15 @@
-from .common import DomainError, Entity
 from .distributor import Distributor, DistributorProps
 from .product import *
 from .shipment import Shipment, ShipmentProps
+from .storage import Storage, StorageProps
 
 __all__ = [
-    "DomainError",
-    "Entity",
     "Product",
     "ProductProps",
     "Distributor",
     "DistributorProps",
     "Shipment",
     "ShipmentProps",
+    "Storage",
+    "StorageProps",
 ]
---
+++ src/domain/entities/storage.py
@@ -0,0 +1,35 @@
+from typing import TypedDict
+
+from src.domain.common import DomainError, Entity
+
+
+class StorageProps(TypedDict):
+    local: dict[
+        {
+            "state": str,
+            "city": str,
+            "address": str,
+            "cep": str,
+        }
+    ]
+    capacity: int
+    items: list[
+        dict[
+            {
+                "product_id": str,
+                "quantity": int,
+            }
+        ]
+    ]
+
+
+class Storage(Entity[StorageProps]):
+    def __init__(self, props: StorageProps, id: str) -> None:
+        super().__init__(props, id)
+
+    @staticmethod
+    def create(props: StorageProps, id: str):
+        if props["capacity"] <= 0:
+            raise DomainError("Storage", "Storage capacity must be greater than 0")
+
+        return Storage(props, id)
