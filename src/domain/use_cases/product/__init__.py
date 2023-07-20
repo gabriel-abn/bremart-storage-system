8ac827714ab79f080264937bb300b3ad503106fb
gabriel-abn <gabriel.ab.nascimento@gmail.com>
2023-07-20 20:04:18 -0300

    feat(RegisterProduct): :sparkles: use case base abstract class

---
+++ src/domain/use_cases/product/__init__.py
@@ -0,0 +1,7 @@
+from .register_product import (
+    RegisterProduct,
+    RegisterProductParams,
+    RegisterProductResult,
+)
+
+__all__ = ["RegisterProductParams", "RegisterProductResult", "RegisterProduct"]
---
+++ src/domain/use_cases/product/register_product.py
@@ -0,0 +1,19 @@
+from abc import abstractmethod
+from typing import TypedDict
+
+from src.domain.entities import ProductProps
+from src.domain.protocols import UseCase
+
+
+class RegisterProductParams(TypedDict):
+    product: ProductProps
+
+
+class RegisterProductResult(TypedDict):
+    crypted_id: str
+
+
+class RegisterProduct(UseCase[RegisterProductParams, RegisterProductResult]):
+    @abstractmethod
+    def execute(self, params: RegisterProductParams) -> RegisterProductResult:
+        pass
