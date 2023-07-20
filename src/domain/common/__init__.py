6955ea1d7e674047544d0f41bb16e573368c0e54
gabriel-abn <gabriel.ab.nascimento@gmail.com>
2023-07-20 09:54:55 -0300

    feat(Domain): :sparkles: domain error values and module paths

---
+++ src/domain/common/__init__.py
@@ -0,0 +1,4 @@
+from .domain_error import DomainError
+from .entity import Entity
+
+__all__ = ["DomainError", "Entity"]
--- src/domain/common/domain_error.py
+++ src/domain/common/domain_error.py
@@ -1,6 +1,9 @@
 class DomainError(Exception):
-    def __init__(self, message: str) -> None:
+    def __init__(self, name: str, message: str) -> None:
+        self.args = (name, message)
         self.message = message
+        self.__class__.__qualname__ = name
+        self.__class__.__name__ = name
 
     def __str__(self) -> str:
         return self.message
