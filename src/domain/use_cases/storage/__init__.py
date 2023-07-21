f693968b073da8ce19c69cd1abe18a6d98736c23
gabriel-abn <gabriel.ab.nascimento@gmail.com>
2023-07-21 11:22:17 -0300

    feat(RegisterStorage): :sparkles: use case base abstract class

---
+++ src/domain/use_cases/storage/__init__.py
@@ -0,0 +1,7 @@
+from .register_storage import (
+    RegisterStorage,
+    RegisterStorageParams,
+    RegisterStorageResult,
+)
+
+__all__ = ["RegisterStorage", "RegisterStorageParams", "RegisterStorageResult"]
---
+++ src/domain/use_cases/storage/register_storage.py
@@ -0,0 +1,19 @@
+from abc import abstractmethod
+from typing import TypedDict
+
+from src.domain.entities import StorageProps
+from src.domain.protocols import UseCase
+
+
+class RegisterStorageParams(TypedDict):
+    storage: StorageProps
+
+
+class RegisterStorageResult(TypedDict):
+    crypted_id: str
+
+
+class RegisterStorage(UseCase[RegisterStorageParams, RegisterStorageResult]):
+    @abstractmethod
+    def execute(self, params: RegisterStorageParams) -> RegisterStorageResult:
+        pass
