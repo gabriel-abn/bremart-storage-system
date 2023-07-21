4c63ea80feafd5c67d2fe2b6d49cc93a8632ed90
gabriel-abn <gabriel.ab.nascimento@gmail.com>
2023-07-21 11:28:43 -0300

    feat(GetStorageById): :sparkles: use case base abstract class

--- src/domain/use_cases/storage/__init__.py
+++ src/domain/use_cases/storage/__init__.py
@@ -1,7 +1,21 @@
+from .get_storage_by_id import (
+    GetStorageById,
+    GetStorageByIdParams,
+    GetStorageByIdResult,
+)
 from .register_storage import (
     RegisterStorage,
     RegisterStorageParams,
     RegisterStorageResult,
 )
 
-__all__ = ["RegisterStorage", "RegisterStorageParams", "RegisterStorageResult"]
+__all__ = [
+    # RegisterStorage
+    "RegisterStorage",
+    "RegisterStorageParams",
+    "RegisterStorageResult",
+    # GetStorageById
+    "GetStorageById",
+    "GetStorageByIdParams",
+    "GetStorageByIdResult",
+]
---
+++ src/domain/use_cases/storage/get_storage_by_id.py
@@ -0,0 +1,19 @@
+from abc import abstractmethod
+from typing import TypedDict
+
+from src.domain.entities.storage import StorageProps
+from src.domain.protocols.use_case import UseCase
+
+
+class GetStorageByIdParams(TypedDict):
+    id: str
+
+
+class GetStorageByIdResult(TypedDict):
+    storage: StorageProps
+
+
+class GetStorageById(UseCase[GetStorageByIdParams, GetStorageByIdResult]):
+    @abstractmethod
+    def execute(self, params: GetStorageByIdParams) -> GetStorageByIdResult:
+        pass
